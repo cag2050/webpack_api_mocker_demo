e6382ea202ae683f96971276abfd83887fe2116b
chen_anguo <cag2050@163.com>
2018-11-23 15:44:56 +0800

    m

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+        路由列表：
+        <router-link to='/'>首页</router-link> |
+        <router-link to='/'>首页</router-link> |
+        <router-view/>
+    </div>
 </template>
 
 <script>
@@ -12,12 +14,11 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 </style>
